791e6215e52d564f8f574fc32cd38d48c407f110
John Montgomery <lilspikey@gmail.com>
2009-02-15 19:14:27 +0000

    fleshing out c data types for USB interfaces. JM
    git-svn-id: http://iolab.svn.beanstalkapp.com/iolab/trunk/python@18 71f8c1b2-6f87-4638-bd5e-d7580a58d9d7

--- psyscopex.py
+++ psyscopex.py
@@ -8,10 +8,15 @@ the same way as for a HID device
 
 import logging
 
+from ctypes import *
+
 from hid import HIDDevice
+from hid.cparser import parse, define
 
 try:
-    from hid.osx import IOObjectRelease, find_usb_devices
+    from hid.osx import IOObjectRelease, find_usb_devices, COMObjectRef, IOCreatePlugInInterfaceForService, \
+                        CFUUIDGetConstantUUIDWithBytes, IOCFPlugInInterfaceStruct, SInt32, kIOCFPlugInInterfaceID, \
+                        IUNKNOWN_C_GUTS
     on_osx=True
 except:
     on_osx=False
@@ -19,6 +24,97 @@ except:
 kIOUSBDeviceClassName="IOUSBDevice"
 kUSBVendorID="idVendor"
 kUSBProductID="idProduct"
+
+kIOUSBDeviceUserClientTypeID=CFUUIDGetConstantUUIDWithBytes(None,
+    0x9d, 0xc7, 0xb7, 0x80, 0x9e, 0xc0, 0x11, 0xD4,
+    0xa5, 0x4f, 0x00, 0x0a, 0x27, 0x05, 0x28, 0x61)
+
+class IOUSBDevRequest(Structure):
+    _fields_=[
+        parse('UInt8 bmRequestType').cstruct,
+        parse('UInt8 bRequest').cstruct,
+        parse('UInt16 wValue').cstruct,
+        parse('UInt16 wIndex').cstruct,
+        parse('UInt16 wLength').cstruct,
+        parse('void *pData').cstruct,
+        parse('UInt32 wLenDone').cstruct,
+    ]
+
+define('IOUSBDevRequest',IOUSBDevRequest)
+
+IOAsyncCallback1=parse('void ( *IOAsyncCallback1)(void *refcon,IOReturn result,void *arg0)').ctype
+define('IOAsyncCallback1',IOAsyncCallback1)
+
+class IOUSBIsocFrame(Structure): 
+    _fields_=[
+        parse('IOReturn frStatus').cstruct,
+        parse('UInt16 frReqCount').cstruct,
+        parse('UInt16 frActCount').cstruct,
+    ]
+
+define('IOUSBIsocFrame',IOUSBIsocFrame)
+
+class IOUSBDevRequestTO(Structure):
+    _fields_=[
+        parse('UInt8 bmRequestType').cstruct,
+        parse('UInt8 bRequest').cstruct,
+        parse('UInt16 wValue').cstruct,
+        parse('UInt16 wIndex').cstruct,
+        parse('UInt16 wLength').cstruct,
+        parse('void *pData').cstruct,
+        parse('UInt32 wLenDone').cstruct,
+        parse('UInt32 noDataTimeout').cstruct,
+        parse('UInt32 completionTimeout').cstruct,
+    ]
+
+define('IOUSBDevRequestTO', IOUSBDevRequestTO)
+
+class IOUSBInterfaceInterface182(Structure):
+    _fields_ = IUNKNOWN_C_GUTS + \
+    [
+        parse('IOReturn (*CreateInterfaceAsyncEventSource)(void *self, CFRunLoopSourceRef *source)').cstruct,
+        parse('CFRunLoopSourceRef (*GetInterfaceAsyncEventSource)(void *self)').cstruct,
+        parse('IOReturn (*CreateInterfaceAsyncPort)(void *self, mach_port_t *port)').cstruct,
+        parse('mach_port_t (*GetInterfaceAsyncPort)(void *self)').cstruct,
+        parse('IOReturn (*USBInterfaceOpen)(void *self)').cstruct,
+        parse('IOReturn (*USBInterfaceClose)(void *self)').cstruct,
+        parse('IOReturn (*GetInterfaceClass)(void *self, UInt8 *intfClass)').cstruct,
+        parse('IOReturn (*GetInterfaceSubClass)(void *self, UInt8 *intfSubClass)').cstruct,
+        parse('IOReturn (*GetInterfaceProtocol)(void *self, UInt8 *intfProtocol)').cstruct,
+        parse('IOReturn (*GetDeviceVendor)(void *self, UInt16 *devVendor)').cstruct,
+        parse('IOReturn (*GetDeviceProduct)(void *self, UInt16 *devProduct)').cstruct,
+        parse('IOReturn (*GetDeviceReleaseNumber)(void *self, UInt16 *devRelNum)').cstruct,
+        parse('IOReturn (*GetConfigurationValue)(void *self, UInt8 *configVal)').cstruct,
+        parse('IOReturn (*GetInterfaceNumber)(void *self, UInt8 *intfNumber)').cstruct,
+        parse('IOReturn (*GetAlternateSetting)(void *self, UInt8 *intfAltSetting)').cstruct,
+        parse('IOReturn (*GetNumEndpoints)(void *self, UInt8 *intfNumEndpoints)').cstruct,
+        parse('IOReturn (*GetLocationID)(void *self, UInt32 *locationID)').cstruct,
+        parse('IOReturn (*GetDevice)(void *self, io_service_t *device)').cstruct,
+        parse('IOReturn (*SetAlternateInterface)(void *self, UInt8 alternateSetting)').cstruct,
+        parse('IOReturn (*GetBusFrameNumber)(void *self, UInt64 *frame, AbsoluteTime *atTime)').cstruct,
+        parse('IOReturn (*ControlRequest)(void *self, UInt8 pipeRef, IOUSBDevRequest *req)').cstruct,
+        parse('IOReturn (*ControlRequestAsync)(void *self, UInt8 pipeRef, IOUSBDevRequest *req, IOAsyncCallback1 callback, void *refCon)').cstruct,
+        parse('IOReturn (*GetPipeProperties)(void *self, UInt8 pipeRef, UInt8 *direction, UInt8 *number, UInt8 *transferType, UInt16 *maxPacketSize, UInt8 *interval)').cstruct,
+        parse('IOReturn (*GetPipeStatus)(void *self, UInt8 pipeRef)').cstruct,
+        parse('IOReturn (*AbortPipe)(void *self, UInt8 pipeRef)').cstruct,
+        parse('IOReturn (*ResetPipe)(void *self, UInt8 pipeRef)').cstruct,
+        parse('IOReturn (*ClearPipeStall)(void *self, UInt8 pipeRef)').cstruct,
+        parse('IOReturn (*ReadPipe)(void *self, UInt8 pipeRef, void *buf, UInt32 *size)').cstruct,
+        parse('IOReturn (*WritePipe)(void *self, UInt8 pipeRef, void *buf, UInt32 size)').cstruct,
+        parse('IOReturn (*ReadPipeAsync)(void *self, UInt8 pipeRef, void *buf, UInt32 size, IOAsyncCallback1 callback, void *refcon)').cstruct,
+        parse('IOReturn (*WritePipeAsync)(void *self, UInt8 pipeRef, void *buf, UInt32 size, IOAsyncCallback1 callback, void *refcon)').cstruct,
+        parse('IOReturn (*ReadIsochPipeAsync)(void *self, UInt8 pipeRef, void *buf, UInt64 frameStart, UInt32 numFrames, IOUSBIsocFrame *frameList,'
+                                      'IOAsyncCallback1 callback, void *refcon)').cstruct,
+        parse('IOReturn (*WriteIsochPipeAsync)(void *self, UInt8 pipeRef, void *buf, UInt64 frameStart, UInt32 numFrames, IOUSBIsocFrame *frameList,'
+                                      'IOAsyncCallback1 callback, void *refcon)').cstruct,
+        parse('IOReturn (*ControlRequestTO)(void *self, UInt8 pipeRef, IOUSBDevRequestTO *req)').cstruct,
+        parse('IOReturn (*ControlRequestAsyncTO)(void *self, UInt8 pipeRef, IOUSBDevRequestTO *req, IOAsyncCallback1 callback, void *refCon)').cstruct,
+        parse('IOReturn (*ReadPipeTO)(void *self, UInt8 pipeRef, void *buf, UInt32 *size, UInt32 noDataTimeout, UInt32 completionTimeout)').cstruct,
+        parse('IOReturn (*WritePipeTO)(void *self, UInt8 pipeRef, void *buf, UInt32 size, UInt32 noDataTimeout, UInt32 completionTimeout)').cstruct,
+        parse('IOReturn (*ReadPipeAsyncTO)(void *self, UInt8 pipeRef, void *buf, UInt32 size, UInt32 noDataTimeout, UInt32 completionTimeout, IOAsyncCallback1 callback, void *refcon)').cstruct,
+        parse('IOReturn (*WritePipeAsyncTO)(void *self, UInt8 pipeRef, void *buf, UInt32 size, UInt32 noDataTimeout, UInt32 completionTimeout, IOAsyncCallback1 callback, void *refcon)').cstruct,
+        parse('IOReturn (*USBInterfaceGetStringIndex)(void *self, UInt8 *si)').cstruct,
+    ]
 
 def find_devices():
     # attempt to directly find usb devices via
@@ -44,6 +140,24 @@ class PsyScopeXUSBDevice(HIDDevice):
         if self._usbDevice:
             self.logging_info("releasing USB device: %s"%self)
             self.IOObjectRelease(self._usbDevice)
+    
+    def open(self):
+        logging.info("opening hid device via usb")
+        # plugInInterface initialised to NULL
+        plugInInterface=COMObjectRef(POINTER(POINTER(IOCFPlugInInterfaceStruct))())
+        score=SInt32()
+        IOCreatePlugInInterfaceForService(self._usbDevice, kIOUSBDeviceUserClientTypeID,
+            kIOCFPlugInInterfaceID, byref(plugInInterface.ref), byref(score));
+        
+        
+        # query to get the HID interface
+        #hidInterface=POINTER(POINTER(IOHIDDeviceInterface122))()
+        #plugInInterface.QueryInterface(CFUUIDGetUUIDBytes(kIOHIDDeviceInterfaceID),parse('LPVOID*').cast(byref(hidInterface)))
+        
+        #self._hidInterface=COMObjectRef(hidInterface)
+        
+        # open the HID device
+        #self._hidInterface.open(0)
 
 if __name__ == '__main__':
     for dev in find_devices():
