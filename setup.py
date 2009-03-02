4c1e2443a841eb3b9ccca5190e8001eb0e096372
John Montgomery <lilspikey@gmail.com>
2009-03-02 12:29:32 +0000

    added setup.py script to make installing and generating source packages etc easier.  moved text from INSTALL.txt to top of README.txt as that gets automatically bundled into package when source built.
    git-svn-id: http://iolab.svn.beanstalkapp.com/iolab/trunk/python@25 71f8c1b2-6f87-4638-bd5e-d7580a58d9d7

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ioLab',
+      version='3.2',
+      description='ioLab response box library',
+      author='John Montgomery',
+      url='http://www.ioLab.co.uk/',
+      py_modules=['ioLabs', 'psyscopex'],
+      packages=['hid'],
+     )
